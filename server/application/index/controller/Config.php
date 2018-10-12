344e6902e082735f2492e186345f976eaa2a4980
祈 <yuqi2060@163.com>
2018-10-12 14:57:40 +0800

    add some page

---
+++ server/application/index/controller/Config.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\index\controller;
+
+class Config
+{	
+	
+    public function regSetting(){
+
+    }
+}
---
+++ server/application/index/controller/Message.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+
+class Message
+{
+    
+	public function index(){
+		//用户信息
+		return view('/message');
+	}
+
+}
