b7adf62e23665261abf6d7173d36338527c42154
祈 <yuqi2060@163.com>
2018-10-16 14:13:55 +0800

    entend

---
+++ server/extend/ShortMessage.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/9
+ * Time: 19:02
+ */
+
+
+
+use Aliyun\DySDKLite\SignatureHelper;
+use \think\Config;
+
+class ShortMessage
+{
+    /**
+     * 发送短信
+     */
+    public function sendSms($phone, $code)
+    {
+        require_once (ROOT_PATH  . 'extend/aliyun/SignatureHelper.php');
+        $params = array();
+        // *** 需用户填写部分 ***
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = Config::get('sendMessage.accessKeyId');
+        $accessKeySecret = Config::get('sendMessage.accessKeySecret');
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $phone;
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = Config::get('sendMessage.signName');
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = Config::get('sendMessage.templateCode');
+        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = Array(
+            "code" => $code,
+            // "product" => "阿里通信"
+        );
+        // fixme 可选: 设置发送短信流水号
+        $params['OutId'] = "12345";
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        //$params['SmsUpExtendCode'] = "1234567";
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+        return $content;
+    }
+//ini_set("display_errors", "on"); // 显示错误提示，仅用于测试时排查问题
+// error_reporting(E_ALL); // 显示所有错误提示，仅用于测试时排查问题
+//set_time_limit(0); // 防止脚本超时，仅用于测试使用，生产环境请按实际情况设置
+//header("Content-Type: text/plain; charset=utf-8"); // 输出为utf-8的文本格式，仅用于测试
+// 验证发送短信(SendSms)接口
+//print_r(sendSms($phone='',$code='123456'));
+}
---
+++ server/extend/duanxin/demo/sendSms.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * 此文件用于验证短信服务API接口，供开发时参考
+ * 执行验证前请确保文件为utf-8编码，并替换相应参数为您自己的信息，并取消相关调用的注释
+ * 建议验证前先执行Test.php验证PHP环境
+ *
+ * 2017/11/30
+ */
+
+namespace duanxin\demo;
+
+//require_once "../SignatureHelper.php";
+
+use duanxin\SignatureHelper;
+
+
+/**
+ * 发送短信
+ */
+class sendSms{
+    function sendSms($code,$phone) {
+
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = "LTAI3NfY6JVzdFI7";
+        $accessKeySecret = "uyAiiBCMMBAyDmkjo59TZOXhrhHNHP";
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $phone;
+
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = "魔方科技";
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = "SMS_137865176";
+//
+//        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = Array (
+            "code" => $code,
+//            "product" => "阿里通信"
+        );
+
+        // fixme 可选: 设置发送短信流水号
+        $params['OutId'] = "12345";
+
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        $params['SmsUpExtendCode'] = "1234567";
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+
+        return $content;
+    }
+}
+
+
