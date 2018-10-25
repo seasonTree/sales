a6e41b25dde067e691c67c5161460c4fd7d20003
13538048392 <1642504508@qq.com>
2018-10-25 14:31:23 +0800

    user 增删改查

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -29,7 +29,7 @@ export function resetPassword(data) {
 //获取所有角色
 export function get(data) {
     return request({
-        url: '/role/lst',
+        url: '/user/lst',
         method: 'post',
         data: data
     })
@@ -38,6 +38,22 @@ export function get(data) {
 export function add(data){
     return request({
         url: '/user/add',
+        method: 'post',
+        data: data
+    })
+}
+//删除账号
+export function del(data){
+    return request({
+        url: '/user/del',
+        method: 'post',
+        data: data
+    })
+}
+//修改账号
+export function edit(data){
+    return request({
+        url: '/user/edit',
         method: 'post',
         data: data
     })
--- client/src/js/user_manage.js
+++ client/src/js/user_manage.js
@@ -11,6 +11,7 @@ new Vue({
         let that = this;
         that.$api.user.get().then((data) =>{
             that.rolesAll = data.data;
+            that.tdata = data.Userdata;
         }).catch((data) =>{
             console.log('eeeeeeeeeeeee');
             console.log(data);
@@ -23,33 +24,38 @@ new Vue({
             show_pass: false,
             show_pass1: false,
             rolesAll:{},
+            userAll:{},
             addData:{
                 role_id:[],
             },
+            editData:{
+                role_id:[],
+            },
             showAdd: false,
+            showEdit: false,
 
             theader: [{
                     text: 'ID',
                     align: 'left',
-                    value: 'a',
+                    value: 'id',
                     sortable: false,
                 },
                 {
                     text: '登录名',
                     align: 'left',
-                    value: 'b',
+                    value: 'username',
                     sortable: false,
                 },
                 {
                     text: '角色',
                     align: 'right',
-                    value: 'c',
+                    value: 'role_name',
                     sortable: false,
                 },
                 {
                     text: '是否启用',
                     align: 'left',
-                    value: 'd',
+                    value: 'status',
                     sortable: false,
                 },
                 {
@@ -59,12 +65,7 @@ new Vue({
                     sortable: false,
                 },
             ],
-            tdata: [{
-                a: '1',
-                b: 'admin',
-                c: '管理员',
-                d: '是',
-            }]
+            tdata: []
         };
     },
     methods :{
@@ -77,21 +78,71 @@ new Vue({
                 data: that.addData
             }).then((res) => {
 
-                // that.showAdd = false;
-                console.log(res);
-                // that.message.text = res.message;
-                // that.message.color = 'success';
-                // that.message.show = true;
-                // setTimeout(function () {
-                //     window.location.reload();
-                // },2000)
+                that.showAdd = false;
+                that.$comp.toast({
+                    text: res.message,
+                });
+                if(res.error ==0){
+                    setTimeout(function () {
+                        window.location.reload();
+                    },2000)
+                }
             }).catch((data) =>{ //function(data){}
+                that.$comp.toast({
+                    text: data.message,
+                    color:'error',
+                });
 
-                that.message.text = data.message;
-                that.message.color = 'error';
-                that.message.show = true;
-                that.submitLoading = false;
             });
+        },
+        del(id){
+            let that = this;
+            that.$api.user.del({
+                data: id
+            }).then((data) => {
+
+                that.$comp.toast({
+                    text: data.message,
+                });
+
+                setTimeout(function () {
+                    window.location.reload();
+                },2000)
+
+            }).catch((data) =>{ //function(data){}
+                that.$comp.toast({
+                    text: data.message,
+                    color:'error',
+                });
+            });
+        },
+        edit (item) {
+            item.role_id =item.role_id ? item.role_id.split(',').map(item =>{
+                return parseInt(item, 10)
+            }):null;
+            this.editData = item;
+            this.showEdit = true;
+        },
+        editCommit(){
+            let that = this;
+            that.$api.user.edit({
+                data: that.editData
+            }).then((data) => {
+
+                that.showEdit = false;
+                that.$comp.toast({
+                    text: data.message,
+                });
+                setTimeout(function () {
+                    window.location.reload();
+                },2000)
+            }).catch((data) =>{ //function(data){}
+                that.$comp.toast({
+                    text: data.message,
+                    color:'error',
+                });
+            });
+
         },
     }
 })
