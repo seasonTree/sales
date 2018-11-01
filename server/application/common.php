6b43d514f170a81ca022e9cf6d4595902f92b0ab
hannan <langxianwuxue@.163.com>
2018-11-01 19:23:46 +0800

    han

--- server/application/common.php
+++ server/application/common.php
@@ -70,8 +70,8 @@ function getClientIp()
         $ip = getenv("HTTP_CLIENT_IP");
     else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
         $ip = getenv("HTTP_X_FORWARDED_FOR");
-    else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
-        $ip = getenv("REMOTE_ADDR");
+    else if (getenv("REMOTE_ADDR",true) && strcasecmp(getenv("REMOTE_ADDR",true), "unknown"))
+        $ip = getenv("REMOTE_ADDR",true);
     else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
         $ip = $_SERVER['REMOTE_ADDR'];
     else
