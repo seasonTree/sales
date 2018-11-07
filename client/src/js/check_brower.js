66dba5960dbd51da5de9a05973165125f342aa7d
seasonTree <leung_chikin@outlook.com>
2018-11-07 16:57:15 +0800

    浏览器检测

---
+++ client/src/js/check_brower.js
@@ -0,0 +1,25 @@
+// 获取当前浏览器名 及 版本号
+function appInfo() {
+    var browser = {
+            appname: 'unknown',
+            version: 0
+        },
+        userAgent = window.navigator.userAgent.toLowerCase(); // 使用navigator.userAgent来判断浏览器类型
+    //msie,firefox,opera,chrome,netscape  
+    if (/(msie|firefox|opera|chrome|netscape)\D+(\d[\d.]*)/.test(userAgent)) {
+        browser.appname = RegExp.$1;
+        browser.version = RegExp.$2;
+    } else if (/version\D+(\d[\d.]*).*safari/.test(userAgent)) { // safari  
+        browser.appname = 'safari';
+        browser.version = RegExp.$2;
+    }
+    return browser;
+}
+
+let browser = appInfo(),
+    name = browser.appname,
+    isIE = browser.indexOf('msie');
+
+if(isIE){
+    window.location.replace('/browser_choose');
+}
