370a298049f8a8e5bea318b650c9ba5fe0fd2194
seasonTree <leung_chikin@outlook.com>
2018-11-14 09:48:34 +0800

    管理员

--- client/src/js/member_list.js
+++ client/src/js/member_list.js
@@ -20,8 +20,10 @@ new Vue({
         that.$api.user.get().then((data) =>{
             that.rolesAll = data.data;
         }).catch((data) =>{
-            console.log('eeeeeeeeeeeee');
-            console.log(data);
+            that.$comp.toast({
+                text: '获取数据失败，请刷新后重试.',
+                color: 'error'
+            });
         });
     },
 
@@ -50,6 +52,7 @@ new Vue({
                     value: 2
                 },
             ],
+
             showAdd: false,
             addData:{
                 role_id:[],
@@ -58,11 +61,6 @@ new Vue({
             show_pass1: false,
             rolesAll:{},
             currentDataType: 2,
-
-            dateFm: null,
-            dateTo: null,
-            menu: false,
-            menu1: false,
 
             theader: [{
                     text: 'ID',
@@ -115,74 +113,7 @@ new Vue({
 
             tableLoading: false,
 
-            // memberHead: [
-            //     {
-            //         text: 'ID',
-            //         align: 'left',
-            //         value: 'id',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'UserName',
-            //         align: 'left',
-            //         value: 'username',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'First Name',
-            //         align: 'left',
-            //         value: 'first_name',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Last Name',
-            //         align: 'left',
-            //         value: 'last_name',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Phone',
-            //         align: 'left',
-            //         value: 'phone',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Create Time',
-            //         align: 'left',
-            //         value: 'create_time',
-            //         sortable: false,
-            //     }
-            // ],
-
-            // registrationHead: [{
-            //         text: 'Firt Name',
-            //         align: 'left',
-            //         value: 'firstName',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Phone',
-            //         align: 'left',
-            //         value: 'phone',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Ref. Code',
-            //         align: 'left',
-            //         value: 'referralCode',
-            //         sortable: false,
-            //     },
-            //     {
-            //         text: 'Date',
-            //         align: 'left',
-            //         value: 'created_at',
-            //         sortable: false,
-            //     }
-            // ],
-
             tdata: [],
-
-            dataTimer: null
         };
     },
 
@@ -192,15 +123,6 @@ new Vue({
             handler(newValue, oldValue) {
 
                 let that = this;
-                // that.theader = that.memberHead;
-                // if (newValue == 2) {
-                //     that.theader = that.transactionHead;
-                // } else if (newValue == 1) {
-                //     that.theader = that.registrationHead;
-                // }else if (newValue == 3) {
-                //     that.theader = that.registrationHead;
-                // }
-
                 if (this.firstLoading) {
                     return;
                 }
@@ -211,36 +133,13 @@ new Vue({
             },
             immediate: true
         },
-
-        // dateFm(newValue, oldValue) {
-        //     if(this.firstLoading){
-        //         return;
-        //     }
-        //
-        //     this.getReomteData();
-        // },
-        //
-        // dateTo(newValue, oldValue) {
-        //     if(this.firstLoading){
-        //         return;
-        //     }
-        //
-        //     this.getReomteData();
-        // },
-
-        // 'pager.index'(){            
-        //     this.getReomteData();
-        // }
     },
 
     methods: {
-        exportExcel() {
 
-        },
         addCommit(){
             let that = this,
                 data={};
-            // console.log(that.addData);
 
             that.$api.user.addDls({
                 data: that.addData
@@ -285,50 +184,24 @@ new Vue({
             });
 
         },
-        changePage() {
-            this.getReomteData();
-        },
+        // changePage() {
+        //     this.getReomteData();
+        // },
 
         getReomteData() {
             let that = this;
-
-            // 发送数据类型、
-            // {
-            //     data: {
-            //         type: 0 | 1 , 0交易报告， 1注册历史
-            //         dateFm: '开始时间'
-            //         dateTo: '结束时间'
-            //         pageSize: 每页显示多少页
-            //         pageIndex: 当前页
-            //     }
-            // }
-
-            //返回数据
-            // {
-            //     data: {
-            //         list: [],
-            //         pageIndex: 1,
-            //         pageCount: 10
-            //     },
-            //     code: 0 | 其他数值
-            //     msg: '消息'
-            // }
 
             that.tableLoading = true;
 
             that.$api.user.getmemberlst({
                 data: {
                     type: that.currentDataType,
-                    // dateFm: that.dateFm,
-                    // dateTo: that.dateTo,
                     pageSize: that.pager.size,
                     pageIndex: that.pager.index
                 }
             }).then((res) => {
                 if (res.code == 0) {
-                    console.log(res);
                     that.tdata = res.data.data;
-                    // that.pager.index = res.data.current_page;
                     that.pager.count = res.data.pageCount || 1;
                 } else {
                     that.$comp.toast({
