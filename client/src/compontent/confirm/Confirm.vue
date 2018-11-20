7e7472f01dfc0bc6ed6345824c80ec226492487f
seasonTree <leung_chikin@outlook.com>
2018-11-20 11:32:50 +0800

    foramt

---
+++ client/src/compontent/confirm/Confirm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "confirm",
+
+    components: {},
+
+    data() {
+        return {};
+    },
+
+    created() {},
+
+    mounted() {},
+
+    computed: {},
+
+    methods: {}
+};
+</script>
+<style lang='less' scoped>
+</style>
