95526738d4866e3ed90f0ed8dbaaf9c0117395e3
seasonTree <leung_chikin@outlook.com>
2018-11-28 18:26:56 +0800

    页面

--- client/src/compontent/toast/toast.vue
+++ client/src/compontent/toast/toast.vue
@@ -1,5 +1,4 @@
 <template>
-<template>
     <v-snackbar :color="color" v-model="visible" :timeout="timeout" :top="top" :left="left" :right="right" :bottom="bottom">
         {{ text }}
         <v-btn color="blue" flat @click="visible = false">
